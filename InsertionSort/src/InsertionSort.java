d7841ffeec010c822ee17070a314fc7f2a0e9c96
Anthony Mace <amace93@gmail.com>
2014-10-30 19:49:25 -0700

    ignores array fill in ecexution time, only counts sort time

--- InsertionSort/src/InsertionSort.java
+++ InsertionSort/src/InsertionSort.java
@@ -8,19 +8,19 @@ import java.util.Random;
 public class InsertionSort {
 
     public static void main(String[] args) {
-        int[] testArray = fillArray(1000);
+        int[] testArray = fillArray(100);
         long startTime = System.nanoTime();
         insertionSort(testArray);
         System.out.println(Arrays.toString(testArray));
         long endTime = System.nanoTime();
-        System.out.println("Total execution time: " + (endTime - startTime) + "ns");
+        System.out.println("Total sort time: " + (endTime - startTime) + "ns");
     }
 
     public static void insertionSort(int[] arrayToSort) {
         for (int i = 1; i < arrayToSort.length; i++) {
             int valueToSort = arrayToSort[i];
             int indexOfValueToMove = i;
-            while (i > 0 && arrayToSort[indexOfValueToMove - 1] > arrayToSort[indexOfValueToMove]) {
+            while (indexOfValueToMove > 0 && arrayToSort[indexOfValueToMove - 1] > valueToSort) {
                 arrayToSort[indexOfValueToMove] = arrayToSort[indexOfValueToMove - 1];
                 indexOfValueToMove--;
             }
