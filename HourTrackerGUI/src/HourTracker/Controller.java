d2c2577f95530e1b9a1ee295bfe5123eec749d0f
Anthony Mace <amace93@gmail.com>
2015-03-07 01:03:31 -0700

    need to figure out how to check input every time

--- HourTrackerGUI/src/HourTracker/Controller.java
+++ HourTrackerGUI/src/HourTracker/Controller.java
@@ -25,22 +25,27 @@ public class Controller {
     public void calcHours(ActionEvent actionEvent) {
         String week1 = week1Hours.getText();
         String week2 = week2Hours.getText();
-        if (week1.length() == 0 || week2.length() == 0) {
-            Alert alertBox = new Alert(Alert.AlertType.INFORMATION);
-            alertBox.setTitle("Information Dialog");
-            alertBox.setHeaderText(null);
-            alertBox.setContentText("Make sure input fields have text!");
-            alertBox.showAndWait();
+        while (week1.length() == 0 || week2.length() == 0) {
+            if (week1.length() == 0 || week2.length() == 0) {
+                Alert alertBox = new Alert(Alert.AlertType.INFORMATION);
+                alertBox.setTitle("Information Dialog");
+                alertBox.setHeaderText(null);
+                alertBox.setContentText("Make sure input fields have text!");
+                alertBox.showAndWait();
+            } else {
+                week1 = week1Hours.getText();
+                week2 = week2Hours.getText();
+                int[] hoursMinutesWeek1 = parseHoursAndMinutesString(week1);
+                int[] hoursMinutesWeek2 = parseHoursAndMinutesString(week2);
+                int addedHours = addHoursOrMinsFromTwoWeeks(hoursMinutesWeek1, hoursMinutesWeek2, true);
+                int addedMins = addHoursOrMinsFromTwoWeeks(hoursMinutesWeek1, hoursMinutesWeek2, false);
+                addedHours = convertMinsToHours(addedHours, addedMins);
+                int convertedMins = leftOverMinutes(addedMins);
+                double decimalTime = hoursToDecimals(addedHours, convertedMins);
+                printHours(decimalTime);
+                writeHoursToFile(decimalTime, convertedMins);
+            }
         }
-        int[] hoursMinutesWeek1 = parseHoursAndMinutesString(week1);
-        int[] hoursMinutesWeek2 = parseHoursAndMinutesString(week2);
-        int addedHours = addHoursOrMinsFromTwoWeeks(hoursMinutesWeek1, hoursMinutesWeek2, true);
-        int addedMins = addHoursOrMinsFromTwoWeeks(hoursMinutesWeek1, hoursMinutesWeek2, false);
-        addedHours = convertMinsToHours(addedHours, addedMins);
-        int convertedMins = leftOverMinutes(addedMins);
-        double decimalTime = hoursToDecimals(addedHours, convertedMins);
-        printHours(decimalTime);
-        writeHoursToFile(decimalTime, convertedMins);
     }
 
     public int[] parseHoursAndMinutesString(String hoursAndMinutes) {
